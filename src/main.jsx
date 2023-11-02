12dc257fb5681f02e600d33f59b7466ae2620cc6
Prosper1218 <wprosper92@gmail.com>
2023-11-02 11:04:19 +0100

    deleted an import

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-
 import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 
